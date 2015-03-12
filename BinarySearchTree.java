3c9c15be41158e5639263831d9932bf81b0241ab
Pankaj Trivedi <pankajk@valyoo.in>
2015-03-12 22:24:05 +0530

    updated BST

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -73,6 +73,18 @@ public class BinarySearchTree {
 		}
 		return root;
 	}
+	
+	public BSTNode convertToBST(int arr[], int start, int end){
+	        if(start<=end){ 
+	        System.out.println("start="+start+" end="+end);
+			int mid = start + (end-start)/2;
+			BSTNode node = new BSTNode(arr[mid]);
+			node.left = convertToBST(arr,start,mid-1);
+			node.right = convertToBST(arr,mid+1,end);
+			return node;
+	      }
+	        return null;
+	}
 	/**
 	 * BFS implementation on the binary search tree
 	 * */
